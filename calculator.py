16fa9e8d9566c9c03d7bf53aea154d24c1eed16c
tousif <54286896+tousif1504@users.noreply.github.com>
2020-02-24 23:59:10 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+a=int(input("enter first number "))
+b=int(input("enter second number "))
+c=a+b
+print("sum of the number is",end=' ')
+print(c)
+d=a-b
+print("sub of the number is",end=' ')
+print(d)
+e=a*b
+print("mul of the number is",end=' ')
+print(e)
+f=a/b
+print("div of the number is",end=' ')
+print(f)
+g=a%b
+print("mod of the number is",end=' ')
+print(g)
+h=a//b
+print("div floor of the number is",end=' ')
+print(h)
